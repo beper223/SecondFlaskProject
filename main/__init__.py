4c02e0ef33be756fb0299f3484c5d1d779b30307
beper223 <zashalovskiy@gmail.com>
2025-06-04 15:15:16 +0200

    2 commit

--- main/__init__.py
+++ main/__init__.py
@@ -3,7 +3,7 @@ from .routes import register_blueprints
 
 def create_app():
     app = Flask(__name__)
-
+    app.secret_key = 'your-secret-key'  # для flash
     # Регистрация blueprint'ов
     register_blueprints(app)
 
--- main/routes/__init__.py
+++ main/routes/__init__.py
@@ -1,6 +1,8 @@
 from .main import main_bp
+from .about import about_bp
 
 def register_blueprints(app):
     app.register_blueprint(main_bp)# url_prefix='/admin'
+    app.register_blueprint(about_bp,url_prefix='/about')
     # app.register_blueprint(admin_bp, url_prefix='/admin')
     # app.register_blueprint(api_bp, url_prefix='/api/v1')
